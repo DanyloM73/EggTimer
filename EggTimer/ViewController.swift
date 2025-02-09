c7a4b658afb93210e597e649654cc91eed4462ac
DanyloM73 <danyamalanin7@gmail.com>
2025-02-09 14:03:44 +0200

    Add timer logic

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    var player: AVAudioPlayer!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer: Timer?
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer?.invalidate()
+        
+        let hardness = sender.currentTitle!
+        if let time = eggTimes[hardness] {
+            infoLabel.text = hardness
+            progressBar.progress = 0.0
+            countdownTimer(time: time)
+        }
+    }
+    
+    func countdownTimer(time: Int) {
+        var countdownTime = time
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            if countdownTime > 0 {
+                countdownTime -= 1
+                self.progressBar.progress = Float(time - countdownTime) / Float(time)
+            } else {
+                timer.invalidate()
+                self.infoLabel.text = "Egg is ready!"
+                self.playSound()
+            }
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
